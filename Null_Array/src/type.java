2c6f1fda863b64a10f7c95793f989613c12cfa6f
hisanori-ito <86972518+hisanori-ito@users.noreply.github.com>
2022-04-07 23:54:09 +0900

    learn null and array, variable types

---
+++ Null_Array/src/type.java
@@ -0,0 +1,24 @@
+public class type {
+  public static void main(String[] args) {
+    long l = 100000000L;
+    double d = 3.14;
+    boolean b = true;
+
+    System.out.println(l);
+    System.out.println(d);
+    System.out.println(b);
+
+    Float f = 6.28f;
+    Integer i = 100;
+    Character c = 'A';
+
+    System.out.println(f);
+    System.out.println(i);
+    System.out.println(c);
+
+    int[] array1 = { 1, 2, 3, 4, 5 };
+    int int_array3 = array1[3];
+
+    System.out.println(int_array3);
+  } 
+}
