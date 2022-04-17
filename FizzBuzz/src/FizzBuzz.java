bb4ad7bca8953aa31cfdddde6d976aba62278540
hisanori-ito <86972518+hisanori-ito@users.noreply.github.com>
2022-04-17 23:30:07 +0900

    FizzBuzz

---
+++ FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,16 @@
+
+public class FizzBuzz {
+	public static void main(String[] args) {
+		for (int number = 1; number <= 30; number++) {
+			if (number%15 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (number%3 == 0) {
+				System.out.println("Fizz");
+			} else if (number%5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(number);
+			}
+		}
+	}
+}
